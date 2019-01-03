2b97846b6c2ddae906a1f0423896edb0f70d5a0a
Lucas Lombardo <Lucasl@bu.edu>
2019-01-03 12:44:25 -0500

    adds footer icons

--- src/components/latitude/Footer/Footer.js
+++ src/components/latitude/Footer/Footer.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faChartArea, faCog, faFile } from '@fortawesome/free-solid-svg-icons'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {
+  faMapMarker,
+  faPhone,
+  faEnvelope,
+} from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
 import NavList from '../NavList/NavList'
 import Container from '../Container/Container'
 import './Footer.scss'
+
+library.add(fab)
 
 const navItems = [
   { text: 'Phasellus sed scelerisque', href: '#' },
@@ -30,22 +38,34 @@ const Footer = () => (
           laoreet. Nunc imperdiet elit sed dolore lacus venenatis rutrum.
         </p>
         <h3>Tellus nunc dapibus</h3>
-        <div>
+        <address>
           <div>
             <li>
-              1234 Somewhere Rd. <br />
-              Nashville, TN 00000
+              <FontAwesomeIcon icon={faMapMarker} />
+              <a href="https://goo.gl/maps/RJ4Cnj8jtS62">
+                1234 Somewhere Rd. <br />
+                Nashville, TN 00000
+              </a>
             </li>
-            <li>(000) 000-0000</li>
+            <li>
+              <FontAwesomeIcon icon={faPhone} />
+              <a href="tel:1-000-000-000">(000) 000-0000</a>
+            </li>
           </div>
           <div>
-            <li>info@untitled.tld</li>
-            <li>@untitledcorp-tld</li>
+            <li>
+              <FontAwesomeIcon icon={faEnvelope} />
+              <a href="mailto:info@untitled.tld">info@untitled.tld</a>
+            </li>
+            <li>
+              <FontAwesomeIcon icon={['fab', 'twitter']} />
+              <a href="https://twitter.com/">@untitledcorp-tld</a>
+            </li>
           </div>
-        </div>
+        </address>
       </div>
     </Container>
-    <p>© Untitled. All rights reserved.</p>
+    <p className="copyright">© Untitled. All rights reserved.</p>
   </div>
 )
 
