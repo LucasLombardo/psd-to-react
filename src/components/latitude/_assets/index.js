0ec29befa2da3e90ee75479ee11925a8f778dd31
Lucas Lombardo <Lucasl@bu.edu>
2019-01-02 21:03:59 -0500

    adds remaining pictures and indexes them

---
+++ src/components/latitude/_assets/index.js
@@ -0,0 +1,12 @@
+import banner from './banner.jpg'
+import pic01 from './pic01.jpg'
+import pic02 from './pic02.jpg'
+import pic03 from './pic03.jpg'
+import pic04 from './pic04.jpg'
+import pic05 from './pic05.jpg'
+import pic06 from './pic06.jpg'
+import pic07 from './pic07.jpg'
+import pic08 from './pic08.jpg'
+import pic09 from './pic09.jpg'
+
+export { banner, pic01, pic02, pic03, pic04, pic05, pic06, pic07, pic08, pic09 }
