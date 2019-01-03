cce5df4a9516b59a1817e0a9443f5c617ccc90ee
Lucas Lombardo <Lucasl@bu.edu>
2019-01-02 21:13:54 -0500

    adds section two content

--- src/components/latitude/MainContent/MainContent.js
+++ src/components/latitude/MainContent/MainContent.js
@@ -3,14 +3,22 @@ import IconList from '../IconList/IconList'
 import Thumbnail from '../Thumbnail/Thumbnail'
 import Container from '../Container/Container'
 import './MainContent.scss'
-import pic01 from '../_assets/pic01.jpg'
-import pic02 from '../_assets/pic02.jpg'
-import pic03 from '../_assets/pic03.jpg'
+import {
+  pic01,
+  pic02,
+  pic03,
+  pic04,
+  pic05,
+  pic06,
+  pic07,
+  pic08,
+  pic09,
+} from '../_assets/index.js'
 
 const MainContent = () => (
   <div id="MainContent">
-    <section id="section-one">
-      <Container>
+    <Container>
+      <section id="section-one">
         <IconList />
         <Thumbnail imgSrc={pic01} head="Nullam arcu consequat" btn="Learn More">
           Phasellus dapibus sed convallis feugiat ipsum. Donec tesque turpis sed
@@ -24,14 +32,38 @@ const MainContent = () => (
           Phasellus dapibus sed convallis feugiat ipsum. Donec tesque turpis sed
           amet ultrices vestibulum nulla.
         </Thumbnail>
-      </Container>
-    </section>
-    <section id="section-two">
-      <div>2</div>
-    </section>
-    <section id="section-three">
-      <div>3</div>
-    </section>
+      </section>
+      <hr />
+      <section id="section-two">
+        <Thumbnail
+          imgSrc={pic04}
+          head="Feugiat phasellus sed consequat"
+          subHead="Lorem ipsum dolor sit amet veroeros"
+          btn="Learn More"
+        >
+          Phasellus dapibus convallis scelerisque. Donec tempus augue id tortor
+          ultricies eget pellentesque turpis ultrices. Vestibulum ut nulla sem,
+          vel iaculis arcu. Nulla vel lorem nisl, quis ultricies tellus. Nunc
+          imperdiet elit sed dolore lacus venenatis rutrum. Vivamus vulputate
+          urna dolor est sagittis purus, in laoreet.
+        </Thumbnail>
+        <Thumbnail
+          imgSrc={pic05}
+          head="Dapibus lorem luctus volutpat rutrum"
+          subHead="Lorem ipsum dolor sit amet veroeros"
+          btn="Learn More"
+        >
+          Phasellus dapibus convallis scelerisque. Donec tempus augue id tortor
+          ultricies eget pellentesque turpis ultrices. Vestibulum ut nulla sem,
+          vel iaculis arcu. Nulla vel lorem nisl, quis ultricies tellus. Nunc
+          imperdiet elit sed dolore lacus venenatis rutrum. Vivamus vulputate
+          urna dolor est sagittis purus, in laoreet.
+        </Thumbnail>
+      </section>
+      <section id="section-three">
+        <div>3</div>
+      </section>
+    </Container>
   </div>
 )
 
