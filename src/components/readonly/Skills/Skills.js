8d11d94c902a7003386a168524a4b7f041f0f82d
Lucas Lombardo <Lucasl@bu.edu>
2019-01-07 13:56:24 -0500

    adds styling for skills

--- src/components/readonly/Skills/Skills.js
+++ src/components/readonly/Skills/Skills.js
@@ -1,21 +1,52 @@
 import React from 'react'
+import Container from '../Container/Container'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faCode,
+  faCubes,
+  faBook,
+  faCoffee,
+  faBolt,
+  faUsers,
+} from '@fortawesome/free-solid-svg-icons'
+import './Skills.scss'
 
 const Skills = () => (
   <section className="skills">
-    <h2>Things I Can Do</h2>
-    <p>
-      Integer eu ante ornare amet commetus vestibulum blandit integer in curae
-      ac faucibus integer non. Adipiscing cubilia elementum integer lorem ipsum
-      dolor sit amet.
-    </p>
-    <ul>
-      <li>Neque sit cep ante</li>
-      <li>Cras augue cep vitae</li>
-      <li>Porttitor ante</li>
-      <li>Augue fusce morbi</li>
-      <li>Augue enim dictum</li>
-      <li>Tempus risus consequat</li>
-    </ul>
+    <Container>
+      <h2>Things I Can Do</h2>
+      <p>
+        Integer eu ante ornare amet commetus vestibulum blandit integer in curae
+        ac faucibus integer non. Adipiscing cubilia elementum integer lorem
+        ipsum dolor sit amet.
+      </p>
+      <ul>
+        <li>
+          <FontAwesomeIcon icon={faCode} />
+          Neque sit cep ante
+        </li>
+        <li>
+          <FontAwesomeIcon icon={faCubes} />
+          Cras augue cep vitae
+        </li>
+        <li>
+          <FontAwesomeIcon icon={faBook} />
+          Porttitor ante
+        </li>
+        <li>
+          <FontAwesomeIcon icon={faCoffee} />
+          Augue fusce morbi
+        </li>
+        <li>
+          <FontAwesomeIcon icon={faBolt} />
+          Augue enim dictum
+        </li>
+        <li>
+          <FontAwesomeIcon icon={faUsers} />
+          Tempus risus consequat
+        </li>
+      </ul>
+    </Container>
   </section>
 )
 
